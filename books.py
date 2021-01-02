cf04f5a6bf4eb2695c7b2b87f03a7b615cc4f593
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-01-02 17:59:35 +0200

    Code source correction with correct outputs for books.py and categories.py

--- books.py
+++ books.py
@@ -2,39 +2,54 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+books = []
+book_title = []
 
 def scrape_book(book_url):
-    """Function designed to scrape infos from book URL
-    Output : 
-    - scraped infos
-    - .csv file with book title"""
+    """Function designed to scrape the following infos from book URL : 
+    - Universal Product Code (UPC)
+    - Title
+    - Price including tax
+    - Price excluding tax
+    - Number available
+    - Product description
+    - Category
+    - Review rating
+    - Picture URL"""
 
     response = requests.get(book_url)
     if response.ok:
         soup = BeautifulSoup(response.text, 'html.parser')
-        information_table = soup.find('table', class_='table table-striped').find_all('td')
-        universal_product_code = information_table[0].text
+        table = soup.find('table', class_='table table-striped').find_all('td')
+        universal_product_code = table[0].text
         title = soup.h1.text.replace(':','-')
-        price_includ_tax = information_table[3].text.replace('Â', '')
-        price_exclud_tax = information_table[2].text.replace('Â', '')
-        number_available = information_table[5].text
+        book_title.append(title)
+        price_includ_tax = table[3].text.replace('Â', '')
+        price_exclud_tax = table[2].text.replace('Â', '')
+        number_available = table[5].text
         if soup.find('div', id='product_description'):
-                product_description = soup.h2.find_next('p').text.replace('âs', "'s").replace('â', '-').replace('â', '"').replace('â', '"')
+            product_description = soup.h2.find_next('p').text.replace('âs', "'s").replace('â', '-').replace('â', '"').replace('â', '"')
         else:
             product_description = "No description available"
         category = soup.find('ul', class_='breadcrumb').find_all('a')[2].text
         review_rating = soup.find('p', class_='star-rating')['class'][1]
         picture_url = soup.find('div', class_='item active').find('img')['src'].replace('../..', 'http://books.toscrape.com')
 
-        book_features = [book_url, universal_product_code, title, price_includ_tax, price_exclud_tax, number_available, product_description, category, review_rating, picture_url]
-        with open(title + '.csv', 'w', newline ='', encoding='utf-8') as csvfile: 
-            writer = csv.writer(csvfile, delimiter=';')
-            writer.writerow(['Product Page URL', 'Universal Product Code (UPC)', 'Title', 'Price Including Tax', 'Price Excluding Tax', 'Availability', 'Product Description', 'Category', 'Review Rating', 'Picture URL'])
-            writer.writerow(book_features)
+        book_features = [book_url, universal_product_code, title, price_includ_tax, price_exclud_tax, number_available, product_description, category, review_rating, picture_url]       
+        books.append(book_features)
 
-            
+def save_book_features(book_url):
+    """Function designed collected infos in a CSV file"""
+    
+    scrape_book(book_url)
+    title = book_title[0]
+    for book in books:
+        csvfile = open(title + '.csv', 'w', newline ='', encoding='utf-8')
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(['Product Page URL', 'Universal Product Code (UPC)', 'Title', 'Price Including Tax', 'Price Excluding Tax', 'Availability', 'Product Description', 'Category', 'Review Rating', 'Picture URL'])
+        writer.writerow(book)  
 
 # Testing function 
 
 if __name__ == '__main__':
-    scrape_book('http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html')
+    save_book_features('http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html')
--- categories.py
+++ categories.py
@@ -3,35 +3,54 @@ from bs4 import BeautifulSoup
 from books import *
 import csv
 
-def scrape_category(category_url):
-	"""Function designed to scrape the URLs of all products
-	for a page of a category"""
 
-	books_urls = []
+categories = []
+books_urls = []
+
+def extract_book_list(category_url):
+	"""Function designed to extract the list of all books in a category"""
+
 	for i in range(1,9):
 		page_no_i = category_url.replace('index', 'page-' + str(i))
 		response = requests.get(page_no_i)
 		if response.ok : 
 			soup = BeautifulSoup(response.text, 'html.parser')
 			category = soup.find('h1').text
+			categories.append(category)
 			books = soup.find_all('h3')
 			for book in books:
-				book_url = book.find('a')['href']
-				book_url = book_url.replace('../../../', 'http://books.toscrape.com/catalogue/')
+				book_url = book.find('a')['href'].replace('../../../', 'http://books.toscrape.com/catalogue/')
 				books_urls.append(book_url)
 
-			txtfile = open(category + '.txt', 'w') 
-			for book_url in books_urls:
-				txtfile.write(book_url + '\n')
-			txtfile.close()
-			
-			txtfile = open(category + '.txt', 'r')
-			for row in txtfile:
-				book_url = row.strip()
-				scrape_book(book_url)
-			txtfile.close()
+def scrape_category(category_url):
+	"""Function designed to scrape the following infos for all books of a category : 
+    - Universal Product Code (UPC)
+    - Title
+    - Price including tax
+    - Price excluding tax
+    - Number available
+    - Product description
+    - Category
+    - Review rating
+    - Picture URL"""
+	
+	extract_book_list(category_url)
+	for book_url in books_urls:
+		scrape_book(book_url)
+
+
+def save_category_features(category_url):
+	"""Function designed collected infos in a CSV file"""
+
+	scrape_category(category_url)
+	category = categories[0]
+	for category in categories:
+		csvfile = open(category + '.csv', 'w', newline ='', encoding='utf-8')
+		writer = csv.writer(csvfile, delimiter=';')
+		writer.writerow(['Product Page URL', 'Universal Product Code (UPC)', 'Title', 'Price Including Tax', 'Price Excluding Tax', 'Availability', 'Product Description', 'Category', 'Review Rating', 'Picture URL'])
+		writer.writerows(books) 
 
 # Testing function
 
 if __name__ == '__main__':
-    scrape_category('http://books.toscrape.com/catalogue/category/books/fiction_10/index.html')
+    save_category_features('http://books.toscrape.com/catalogue/category/books/fiction_10/index.html')
