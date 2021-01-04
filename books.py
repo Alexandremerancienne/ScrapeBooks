f01e7c956476303977f3c248fc4d0d66b3fe7917
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-01-04 19:41:24 +0200

    New docstrings and function definitions for books.py

--- books.py
+++ books.py
@@ -1,3 +1,16 @@
+"""This program aims at scraping book-selling website "Books To Scrape" (http://books.toscrape.com/) to extract the following information for a specific book:
+
+- product_page_url 
+- universal_ product_code (upc) 
+- title price_including_tax 
+- price_excluding_tax 
+- number_available 
+- product_description 
+- category 
+- review_rating 
+- image_url"""
+
+
 import csv
 
 import requests
@@ -5,7 +18,7 @@ from bs4 import BeautifulSoup
 
 
 def get_book_description_from_url(book_url):
-    """Function scraping the following info from book URL : 
+    """Function scraping the following info from book URL :
     - Universal Product Code (UPC)
     - Title
     - Price including tax
@@ -21,31 +34,33 @@ def get_book_description_from_url(book_url):
         soup = BeautifulSoup(response.content, 'html.parser')
         table = soup.find('table', class_='table table-striped').find_all('td')
         universal_product_code = table[0].text
-        title = soup.h1.text.replace(':','-').replace('/','-').replace('"', '').replace('*', '.').replace('?','')
+        title = soup.h1.text.replace(':', '-').replace('/', '-').replace('"', '').replace('*', '.').replace('?', '')
         price_includ_tax = table[3].text
         price_exclud_tax = table[2].text
         number_available = table[5].text
         if soup.find('div', id='product_description'):
-            product_description = soup.h2.find_next('p').text.replace(':','-')
+            product_description = soup.h2.find_next('p').text.replace(':', '-')
         else:
             product_description = "No description available"
         category = soup.find('ul', class_='breadcrumb').find_all('a')[2].text
         review_rating = soup.find('p', class_='star-rating')['class'][1]
         picture_url = soup.find('div', class_='item active').find('img')['src'].replace('../..', 'http://books.toscrape.com')
 
-        return [book_url, universal_product_code, title, price_includ_tax, price_exclud_tax, number_available, product_description, category, review_rating, picture_url]       
+        return [book_url, universal_product_code, title, price_includ_tax, price_exclud_tax, number_available, product_description, category, review_rating, picture_url]
+
 
 def save_book_description_to_csv(book_url):
     """Function saving book description to CSV file"""
-    
+
     book_description = get_book_description_from_url(book_url)
     title = book_description[2]
-    with open(title + '.csv', 'w', newline ='', encoding='utf-8-sig') as csvfile:
+    with open(title + '.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
         writer.writerow(['Product Page URL', 'Universal Product Code (UPC)', 'Title', 'Price Including Tax', 'Price Excluding Tax', 'Availability', 'Product Description', 'Category', 'Review Rating', 'Picture URL'])
-        writer.writerow(book_description)  
+        writer.writerow(book_description)
 
-# Testing function 
+# Testing function
+
 
 if __name__ == '__main__':
     save_book_description_to_csv('http://books.toscrape.com/catalogue/adulthood-is-a-myth-a-sarahs-scribbles-collection_659/index.html')
